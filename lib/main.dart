9288caf141b52669d08a5ae9c0c28870c48d8b28
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-06-01 15:50:10 -0300

    provider state for time options

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pomodoro/pages/pages.dart';
+import 'package:pomodoro/provider/provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,9 +12,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: PomodoroPage(),
+    return ChangeNotifierProvider<TimerService>(
+      create: (context) => TimerService(),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: PomodoroPage(),
+      ),
     );
   }
 }
--- lib/widgets/timer_options.dart
+++ lib/widgets/timer_options.dart
@@ -1,41 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro/provider/provider.dart';
 import 'package:pomodoro/utils/utils.dart';
+import 'package:provider/provider.dart';
 
 class TimerOptions extends StatelessWidget {
   const TimerOptions({super.key});
 
   @override
   Widget build(BuildContext context) {
-    double selectedTime = 1800;
+    final provider = Provider.of<TimerService>(context);
 
     return SingleChildScrollView(
       controller: ScrollController(initialScrollOffset: 240),
       scrollDirection: Axis.horizontal,
       child: Row(
         children: selectTablesTimes.map((time) {
-          return Container(
-            margin: const EdgeInsets.only(left: 10),
-            width: 70,
-            height: 50,
-            decoration: int.parse(time) == selectedTime
-                ? const BoxDecoration(
-                    color: Colors.white,
-                  )
-                : BoxDecoration(
-                    border: Border.all(
-                      width: 3,
-                      color: Colors.white30,
+          return InkWell(
+            onTap: () => provider.selectTimer(double.parse(time)),
+            child: Container(
+              margin: const EdgeInsets.only(left: 10),
+              width: 70,
+              height: 50,
+              decoration: int.parse(time) == provider.selectedTime
+                  ? const BoxDecoration(
+                      color: Colors.white,
+                    )
+                  : BoxDecoration(
+                      border: Border.all(
+                        width: 3,
+                        color: Colors.white30,
+                      ),
                     ),
+              child: Center(
+                child: Text(
+                  (int.parse(time) ~/ 60).toString(),
+                  style: textStyle(
+                    size: 25,
+                    fontWeight: FontWeight.bold,
+                    color: int.parse(time) == provider.selectedTime
+                        ? Colors.redAccent
+                        : Colors.white,
                   ),
-            child: Center(
-              child: Text(
-                (int.parse(time) ~/ 60).toString(),
-                style: textStyle(
-                  size: 25,
-                  fontWeight: FontWeight.bold,
-                  color: int.parse(time) == selectedTime
-                      ? Colors.redAccent
-                      : Colors.white,
                 ),
               ),
             ),
