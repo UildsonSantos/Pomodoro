030a4629d4da9082447a8fd2bebb91d25c5b9218
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-06-01 14:32:36 -0300

    timer options

---
+++ lib/widgets/timer_options.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TimerOptions extends StatelessWidget {
+  const TimerOptions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(),
+    );
+  }
+}
